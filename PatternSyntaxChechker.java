71b2ffb6f08251af65823bfdad7f42817cbfc548
Ege Özfırıncı <ozfrnce@gmail.com>
2023-01-29 15:28:07 +0300

    Create PatternSyntaxChechker.java

---
+++ PatternSyntaxChechker.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+        int q=1;
+     
+		while(q<=testCases){
+            
+			String pattern = in.nextLine() ;
+            
+            try {Pattern pat =Pattern.compile(pattern,1);
+             System.out.printf("Valid"+"%n");
+            }
+            catch(Exception i){
+                System.out.printf("Invalid"+"%n");
+            }
+            
+            
+        
+          	//Write your code
+              q++;
+                 
+                  
+		}in.close();
+	}
+}
+
+
